bba7d947640598f19d88f30775e44f599e6dc0b7
SaiSreeramulu <sreeramaddepalli@gmail.com>
2024-06-17 11:47:12 +0530

    Microservices Projects

---
+++ ReactiveRestCurd/src/test/java/com/example/reactive/crud/ReactiveRestCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactive.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRestCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
